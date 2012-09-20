89baa2a3f6a301a608507f4bf6cfc741e5e06aac
Vegard Storheil Eriksen <zyp@jvnv.net>
2012-09-20 21:23:37 +0200

    Added F1 USB driver.

--- usb/usb.h
+++ usb/usb.h
@@ -1,11 +1,17 @@
 #ifndef USB_H
 #define USB_H
 
-#if defined(STM32F4)
+#if defined(STM32F1)
+#include "f1_usb.h"
+
+static F1_USB_t USB(0x40005c00, 0x40006000);
+
+#elif defined(STM32F4)
 #include "dwc_otg.h"
 
 static DWC_OTG_t OTG_FS(0x50000000);
 static DWC_OTG_t OTG_HS(0x40040000);
+
 #endif
 
 #endif
