754ed2f1658a58680abb6fa440ad9f7b8ad8c078
Vegard Storheil Eriksen <zyp@jvnv.net>
2013-02-17 18:34:57 +0100

    F4: Added more timer instances.

--- timer/timer.h
+++ timer/timer.h
@@ -34,7 +34,14 @@ static TIM_t& TIM6 = *(TIM_t*)0x40001000;
 static TIM_t& TIM7 = *(TIM_t*)0x40001400;
 static TIM_t& TIM8 = *(TIM_t*)0x40013400;
 #elif defined(STM32F4)
+static TIM_t& TIM1 = *(TIM_t*)0x40010000;
 static TIM_t& TIM2 = *(TIM_t*)0x40000000;
+static TIM_t& TIM3 = *(TIM_t*)0x40000400;
+static TIM_t& TIM4 = *(TIM_t*)0x40000800;
+static TIM_t& TIM5 = *(TIM_t*)0x40000c00;
+static TIM_t& TIM6 = *(TIM_t*)0x40001000;
+static TIM_t& TIM7 = *(TIM_t*)0x40001400;
+static TIM_t& TIM8 = *(TIM_t*)0x40010400;
 #endif
 
 #endif
