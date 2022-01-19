ed1731d14aa7036376c7a68a6689f76ff61c0985
Karl Palsson <karlp@tweak.net.au>
2022-01-19 22:53:53 +0000

    syscfg: stm32wb: legacy implementation
    Functional, after much hair tearing.
    
    Signed-off-by: Karl Palsson <karlp@tweak.net.au>

--- syscfg/syscfg.h
+++ syscfg/syscfg.h
@@ -21,4 +21,27 @@ static SYSCFG_t& SYSCFG = *(SYSCFG_t*)0x40010000;
 
 #endif
 
+#if defined(STM32WB)
+
+struct SYSCFG_t {
+	volatile uint32_t MEMRMP;
+	volatile uint32_t CFGR1;
+	volatile uint32_t EXTICR[4];
+	volatile uint32_t SCSR;
+	volatile uint32_t CFGR2;
+	volatile uint32_t SWPR;
+	volatile uint32_t SKR;
+	volatile uint32_t SWPR2;
+	uint32_t _reserved1[53];  // basicallly, vrefbuf is in this slot!
+	volatile uint32_t IMR1;
+	volatile uint32_t IMR2;
+	volatile uint32_t C2IMR1;
+	volatile uint32_t C2IMR2;
+	volatile uint32_t SIPCR;
+};
+
+static SYSCFG_t& SYSCFG = *(SYSCFG_t*)0x40010000;
+
+#endif
+
 #endif
