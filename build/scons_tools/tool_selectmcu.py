2c1cace1b2e3725f8d49bf0dd1f529a8c7508864
Vegard Storheil Eriksen <zyp@jvnv.net>
2022-09-10 16:08:45 +0200

    build: Add -fcoroutines to default CXXFLAGS.

--- build/scons_tools/tool_selectmcu.py
+++ build/scons_tools/tool_selectmcu.py
@@ -31,7 +31,7 @@ def SelectMCU(env, mcu, variant_dir = None):
 		RANLIB = '${TOOLCHAIN}ranlib',
 		
 		CCFLAGS   = Split('-O2 -Wall -ggdb -ffunction-sections -fdata-sections'),
-		CXXFLAGS  = Split('-std=c++20 -fno-exceptions -fno-rtti'),
+		CXXFLAGS  = Split('-std=c++20 -fno-exceptions -fno-rtti -fcoroutines'),
 		ASFLAGS   = Split('-c -x assembler-with-cpp'),
 		LINKFLAGS = Split('-Wall -nostartfiles -Wl,-T${LINK_SCRIPT} -Wl,--gc-sections'),
 		
